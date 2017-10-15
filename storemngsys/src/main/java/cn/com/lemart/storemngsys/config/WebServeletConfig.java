73c6d9c397106e2d434145dff1aa490d1c41906e
jamesziou <653978237@qq.com>
2017-10-15 09:13:10 +0800

    test
    test

--- storemngsys/src/main/java/cn/com/lemart/storemngsys/config/WebServeletConfig.java
+++ storemngsys/src/main/java/cn/com/lemart/storemngsys/config/WebServeletConfig.java
@@ -1,10 +1,14 @@
 package cn.com.lemart.storemngsys.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.alibaba.druid.pool.DruidDataSource;
 
 @Configuration
 @ComponentScan("cn.com.lemart.storemngsys.web.controller")
@@ -12,7 +16,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @PropertySource({ "classspath:/dbConf/jdbc.properties" })
 public class WebServeletConfig {
 
-	@Bean("dataSource")
-	public void getDataSource() {
+	@Bean(value = "dataSource", destroyMethod = "close")
+	public DruidDataSource getDataSource() {
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setUrl("${}");
+		return dataSource;
+	}
+
+	@Bean("transactionManager")
+	@Autowired
+	public DataSourceTransactionManager getTransactionManager(DruidDataSource dataSource) {
+		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+		transactionManager.setDataSource(dataSource);
+		return transactionManager;
 	}
 }
